37a97d7dd2f7ecaf9c700173e3637fd3dbaecb64
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-11-26 20:51:34 -0500

    Update shops.lua

--- data/shops.lua
+++ data/shops.lua
@@ -63,7 +63,8 @@ return {
 		blip = {
 			id = 402, colour = 69, scale = 0.8
 		}, inventory = {
-			{ name = 'lockpick', price = 10 }
+			{ name = 'lockpick', price = 10 },
+			{ name = 'radio', price = 100 }
 		}, locations = {
 			vec3(2748.0, 3473.0, 55.67),
 			vec3(342.99, -1298.26, 32.51)
@@ -104,42 +105,42 @@ return {
 		}
 	},
 
-	PoliceArmoury = {
-		name = 'Police Armoury',
-		groups = shared.police,
-		blip = {
-			id = 110, colour = 84, scale = 0.8
-		}, inventory = {
-			{ name = 'ammo-9', price = 5, },
-			{ name = 'ammo-rifle', price = 5, },
-			{ name = 'WEAPON_FLASHLIGHT', price = 200 },
-			{ name = 'WEAPON_NIGHTSTICK', price = 100 },
-			{ name = 'WEAPON_PISTOL', price = 500, metadata = { registered = true, serial = 'POL' }, license = 'weapon' },
-			{ name = 'WEAPON_CARBINERIFLE', price = 1000, metadata = { registered = true, serial = 'POL' }, license = 'weapon', grade = 3 },
-			{ name = 'WEAPON_STUNGUN', price = 500, metadata = { registered = true, serial = 'POL'} }
-		}, locations = {
-			vec3(451.51, -979.44, 30.68)
-		}, targets = {
-			{ loc = vec3(453.21, -980.03, 30.68), length = 0.5, width = 3.0, heading = 270.0, minZ = 30.5, maxZ = 32.0, distance = 6 }
-		}
-	},
+	-- PoliceArmoury = {
+	-- 	name = 'Police Armoury',
+	-- 	groups = shared.police,
+	-- 	blip = {
+	-- 		id = 110, colour = 84, scale = 0.8
+	-- 	}, inventory = {
+	-- 		{ name = 'ammo-9', price = 5, },
+	-- 		{ name = 'ammo-rifle', price = 5, },
+	-- 		{ name = 'WEAPON_FLASHLIGHT', price = 200 },
+	-- 		{ name = 'WEAPON_NIGHTSTICK', price = 100 },
+	-- 		{ name = 'WEAPON_PISTOL', price = 500, metadata = { registered = true, serial = 'POL' }, license = 'weapon' },
+	-- 		{ name = 'WEAPON_CARBINERIFLE', price = 1000, metadata = { registered = true, serial = 'POL' }, license = 'weapon', grade = 3 },
+	-- 		{ name = 'WEAPON_STUNGUN', price = 500, metadata = { registered = true, serial = 'POL'} }
+	-- 	}, locations = {
+	-- 		vec3(451.51, -979.44, 30.68)
+	-- 	}, targets = {
+	-- 		{ loc = vec3(453.21, -980.03, 30.68), length = 0.5, width = 3.0, heading = 270.0, minZ = 30.5, maxZ = 32.0, distance = 6 }
+	-- 	}
+	-- },
 
-	Medicine = {
-		name = 'Medicine Cabinet',
-		groups = {
-			['ambulance'] = 0
-		},
-		blip = {
-			id = 403, colour = 69, scale = 0.8
-		}, inventory = {
-			{ name = 'medikit', price = 26 },
-			{ name = 'bandage', price = 5 }
-		}, locations = {
-			vec3(306.3687, -601.5139, 43.28406)
-		}, targets = {
+	-- Medicine = {
+	-- 	name = 'Medicine Cabinet',
+	-- 	groups = {
+	-- 		['ambulance'] = 0
+	-- 	},
+	-- 	blip = {
+	-- 		id = 403, colour = 69, scale = 0.8
+	-- 	}, inventory = {
+	-- 		{ name = 'medikit', price = 26 },
+	-- 		{ name = 'bandage', price = 5 }
+	-- 	}, locations = {
+	-- 		vec3(306.3687, -601.5139, 43.28406)
+	-- 	}, targets = {
 
-		}
-	},
+	-- 	}
+	-- },
 
 	BlackMarketArms = {
 		name = 'Black Market (Arms)',
